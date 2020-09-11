cb56fa466dfe1304f931279e798abb054c8f4c19
Nitul Datt <nitul1991@gmail.com>
2020-09-12 00:37:12 +0530

    Added git aliases for the fish shell

---
+++ fish/conf.d/git_aliases.fish
@@ -0,0 +1,7 @@
+
+
+alias gs="git status"
+alias gd="git diff"
+alias gds="git diff --staged"
+#alias gl="git log --pretty='format:%C(auto)%<|(13)%cr %C(cyan)%<|(30)%cn %C(auto)%h %s %d'"
+alias gl="git log --pretty='format:%C(auto)%<|(11)%cs %C(cyan)%<|(23)%cn %C(auto)%h %s %d'"
