a1579083778c35c84314a659912274cf9f969c24
Nitul Datt <nitul1991@gmail.com>
2021-06-20 22:31:34 +0530

    Default to ls in case exa is not installed, when using the ll function

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,14 +1,20 @@
 
 
 function ll $argv
-    exa \
-    --long \
-    --colour=auto \
-    --all \
-    --group-directories-first \
-    --sort name \
-    --group \
-    --time modified \
-    --no-filesize \
-    $argv
+    # Check if exa exists on the system
+    if command -q exa
+        exa \
+        --long \
+        --colour=auto \
+        --all \
+        --group-directories-first \
+        --sort name \
+        --group \
+        --time modified \
+        --no-filesize \
+        --time-style long-iso \
+        $argv
+    else
+        ls -la
+    end
 end
