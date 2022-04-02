f3c3678de6e37bb6888defaa68c65c5c6d7f2441
Nitul Datt <nitul1991@gmail.com>
2022-04-03 00:33:21 +0530

    Add fish shell function to change directiories using fzf

---
+++ fish/.config/fish/functions/fcd.fish
@@ -0,0 +1,22 @@
+
+
+# From https://gist.github.com/rumpelsepp/b1b416f52d6790de1aee
+function fcd --description "Fuzzy change directory"
+    if set -q argv[1]
+        set searchdir $argv[1]
+    else
+        set searchdir $HOME
+    end
+
+    # https://github.com/fish-shell/fish-shell/issues/1362
+    set -l tmpfile (mktemp)
+    find $searchdir \( ! -regex '.*/\..*' \) ! -name __pycache__ -type d | fzf > $tmpfile
+    set -l destdir (cat $tmpfile)
+    rm -f $tmpfile
+
+    if test -z "$destdir"
+        return 1
+    end
+
+    cd $destdir
+end
